7e8186c9844c627555c292d6d30c31015ffaf67f
Atin Bainada <hi@atinb.me>
2023-06-20 10:14:43 +0530

    move colorscheme code to colorschemes.lua

--- init.lua
+++ init.lua
@@ -1,16 +1,4 @@
 require "core.options"
 require "core.keymaps"
 require "core.plugins"
-
-
--- Lua
-local status_ok, theme = pcall(require, "onedark")
-if not status_ok then
-  return
-end
-
-theme.setup {
-  style = "darker"
-}
-
-theme.load()
+require "core.colorschemes"
